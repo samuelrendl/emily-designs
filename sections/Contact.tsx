723f67894d0318e1d4ae3ec045418aabb2bb3be6
Samuel Rendl <samuelrichardrendl@gmail.com>
2024-07-07 12:20:05 +0200

    adding imdb and credits

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -2,18 +2,28 @@ import Link from "next/link";
 
 export default function Contact() {
   return (
-    <div className="flex flex-col items-center font-playfair gap-3 m-auto">
-      <p className="text-5xl">Contact me via</p>
-      <p className="select-all text-2xl">emily.kontu@gmail.com</p>
-      {/* <p className="text-2xl">or</p> */}
-      <Link
-        href={"https://www.instagram.com/emicostumes/?hl=en-gb"}
-        target="_blank"
-        className="underline text-2xl"
-      >
-        Instagram
-      </Link>
-
+    <div className="flex flex-col items-center font-playfair gap-4 m-auto">
+      <p className="text-5xl font-bold">Contact me via</p>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-2xl">emily.kontu@gmail.com</p>
+        <Link
+          href={"https://www.instagram.com/emicostumes/?hl=en-gb"}
+          target="_blank"
+          className="underline text-2xl"
+        >
+          Instagram
+        </Link>
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <p className="text-2xl font-bold">and checkout my</p>
+        <Link
+          href={"https://www.imdb.com/name/nm13903798/?ref_=fn_al_nm_1"}
+          target="_blank"
+          className="underline text-2xl"
+        >
+          IMDb
+        </Link>
+      </div>
     </div>
   );
 }
